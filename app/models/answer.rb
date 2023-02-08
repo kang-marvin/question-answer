3a7a56c5fada7652dcacdb70b309f9af4dec4156
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-02-08 16:01:13 +0300

    Limit the number of answers a question can have to 4

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,10 +1,22 @@
 class Answer < ApplicationRecord
+  LIMIT_PER_QUESTION = 4
+
   belongs_to :question
 
   has_rich_text :content
 
+  before_validation :validate_quota
 
   def content_body
     super(self)
+  end
+
+  private
+
+  def validate_quota
+    return unless self.question
+    if self.question.answers.reload.size >= LIMIT_PER_QUESTION
+      errors.add(:base, 'Association limit exceeded.')
+    end
   end
 end
