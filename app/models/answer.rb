2c36c5eddc4d39ce34f0806922e7a3030f1c5360
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-02-08 16:28:26 +0300

    Set each question has only 1 correct answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,16 +6,28 @@ class Answer < ApplicationRecord
   has_rich_text :content
 
   before_validation :validate_quota
+  after_save :set_only_one_correct_answer_per_question, if: :answer_is_the_correct_solution?
 
   def content_body
     super(self)
   end
 
+  def answer_is_the_correct_solution?
+    is_correct_solution
+  end
+
   private
+
+  def set_only_one_correct_answer_per_question
+    self.question
+      .answers
+      .where.not(id: self&.id)
+      .update_all(is_correct_solution: false)
+  end
 
   def validate_quota
     return unless self.question
-    if self.question.answers.reload.size >= LIMIT_PER_QUESTION
+    if self.question.answers.reload.size > LIMIT_PER_QUESTION
       errors.add(:base, 'Association limit exceeded.')
     end
   end
