cb1fa6fc88a72cbbcd287c4ac2b0f859b588bb69
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-02-08 17:19:36 +0300

    Add countdown timer for each question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,10 @@
 class Question < ApplicationRecord
+  TIMER = {
+    simple: 15, easy: 25,
+    intermediate: 40, hard: 55,
+    expert: 90
+  }.freeze
+
   belongs_to :category
   has_many :answers, dependent: :destroy
 
@@ -6,7 +12,19 @@ class Question < ApplicationRecord
 
   validates :content, presence: true
 
+  enum :difficulty_level, {
+    simple: 0,
+    easy: 1,
+    intermediate: 2,
+    hard: 3,
+    expert: 4
+  }, default: :simple
+
   def content_body
     super(self)
+  end
+
+  def countdown_timer
+    TIMER[difficulty_level.to_sym]
   end
 end
