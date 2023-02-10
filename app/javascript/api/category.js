3cc41b0b18f882965601d6f871f24ddfca4d41a8
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-02-11 01:27:43 +0300

    Add question api call points

--- app/javascript/api/category.js
+++ app/javascript/api/category.js
@@ -2,7 +2,7 @@ import * as httpMethods from "./common/httpMethods";
 import urlUtils from "../utils/urlUtils";
 
 class category {
-  static getCategories(searchParams = {page: 1}) {
+  static getCategories(searchParams = { page: 1 }) {
     const queryString = urlUtils.toQueryString(searchParams);
     return httpMethods.get(`categories?${queryString}`)
   }
---
+++ app/javascript/api/question.js
@@ -0,0 +1,11 @@
+import * as httpMethods from "./common/httpMethods";
+import urlUtils from "../utils/urlUtils";
+
+class question {
+  static getQuestion(params = { identifier: 0 }) {
+    const queryString = urlUtils.toQueryString(params);
+    return httpMethods.get(`question?${queryString}`)
+  }
+}
+
+export default question;
