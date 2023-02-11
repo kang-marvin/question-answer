502087cf244ae6c772888cfe5d940bafb8e64c67
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-02-11 14:14:25 +0300

    Styling the search panel

--- app/javascript/components/helper/SearchPanel.js
+++ app/javascript/components/helper/SearchPanel.js
@@ -22,18 +22,20 @@ const SearchPanel = props => {
   }
 
   return (
-    <div>
-      <InputField
-        queryString={queryString}
-        placeholder={placeholder}
-        handleInputChange={handleInputChange}
-      />
-
-      {/* Submit Button */}
-      <SubmitField
-        label={label}
-        handleSubmit={handleSubmit}
-      />
+    <div class="md:flex md:items-center mb-6 gap-1">
+      <div class="md:w-2/3">
+        <InputField
+          queryString={queryString}
+          placeholder={placeholder}
+          handleInputChange={handleInputChange}
+        />
+      </div>
+      <div class="md:w-1/3">
+        <SubmitField
+          label={label}
+          handleSubmit={handleSubmit}
+        />
+      </div>
     </div>
   )
 }
