85f9257a64b2ca925a76e9623a9accd51d988b73
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-02-13 18:46:44 +0300

    Remove totalEntries because of `count` being used

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   def paginate_dict(object)
     {
-      current_page: object.current_page,
-      next_page: object.next_page,
-      previous_page: object.previous_page,
-      total_pages: object.total_pages,
-      total_entries: object.total_entries
+      currentPage: object.current_page,
+      nextPage: object.next_page,
+      previousPage: object.previous_page,
+      totalPages: object.total_pages,
+      totalEntries: nil #object.total_entries
     }
   end
 end
