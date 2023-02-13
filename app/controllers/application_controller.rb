71958f9fc51101cbaf8595cba5464c4c75616bbc
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-02-13 15:04:17 +0300

    Configure pager_api for pagination and work with ActiveModelSerializer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  def paginate_dict(object)
+    {
+      current_page:   object.current_page,
+      next_page:      object.next_page,
+      previous_page:  object.previous_page,
+      total_pages:    object.total_pages,
+      total_entries:  object.total_entries
+    }
+  end
 end
---
+++ config/initializers/active_model_serializers.rb
@@ -0,0 +1,7 @@
+ActiveModel::Serializer.config.adapter = :json
+
+# ActiveModel::Serializer.root = false
+
+Mime::Type.register 'application/json', :json, %w[text/x-json application/jsonrequest application/vnd.api+json]
+
+ActiveModel::Serializer.config.default_includes = '**'
