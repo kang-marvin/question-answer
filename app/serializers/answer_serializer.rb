b89ef14040c79a334a66f2e71af33386512af2cd
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-02-14 17:00:16 +0300

    Add id to attribute to Answer Serializer

--- app/serializers/answer_serializer.rb
+++ app/serializers/answer_serializer.rb
@@ -1,8 +1,9 @@
 class AnswerSerializer < ActiveModel::Serializer
-  attributes :content_body,
-             :correct?
+  attributes :id,
+             :content_body,
+             :is_correct
 
-  def correct?
+  def is_correct
     object.answer_is_the_correct_solution?
   end
 end
