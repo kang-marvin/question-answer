f352358ec3d89015490e2fa4be43d610b95e13e5
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-02-17 19:15:03 +0300

    Create random testing data for Category, Questions and Answers

---
+++ data/README.md
@@ -0,0 +1,7 @@
+# Data.json
+
+Data is generated randomly from [https://www.mockaroo.com](https://www.mockaroo.com)
+
+The schema is shown in the screenshot.
+
+![Schema Image](schema_used.png "schema image")
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -11,36 +13,44 @@ Question.delete_all
 Category.delete_all
 
 # Create Questions
-def create_questions(category)
-  5.times do |count|
-    question = Question.create({
-                                 category: category,
-                                 difficulty_level: Question::TIMER.keys.sample,
-                                 content: "What is the number #{count} squared?"
-                               })
-    create_answers(question)
+def create_questions(category, questions)
+  questions.each do |question_data|
+    data = {
+      category_id: category.id,
+      difficulty_level: Question::TIMER.keys.sample,
+      content: question_data['content']
+    }
+    question = Question.create(data)
+    create_answers(question, question_data['answers'])
   end
 end
 
 # Create Questions and Answers
-def create_answers(question)
-  correct_answer_index = [1, 2, 3, 4].sample
-
-  4.times do |count|
-    Answer.create({
-                    question: question,
-                    is_correct_solution: (count == correct_answer_index),
-                    content: (count * 3)
-                  })
+def create_answers(question, answers)
+  correct_answer_index = rand(1..4)
+  data = {
+    question_id: question.id
+  }
+  answers.each_with_index do |answer_obj, index|
+    Answer.create(
+      data.merge({
+                   is_correct_solution: (correct_answer_index == index),
+                   content: answer_obj['content']
+                 })
+    )
   end
 end
 
 # Create categories
-[
-  'Java', 'Oracle', 'AWS', 'Ruby on Rails', 'Soft Skills'
-].each do |topic|
+json_file = File.open(Rails.root.join('data', 'data.json'))
+data = JSON.load json_file
+
+puts "> JSON file loaded\n\n"
+
+data.first['categories'].each do |category_data|
   ActiveRecord::Base.transaction do
-    category = Category.create(title: topic)
-    create_questions(category)
+    category = Category.create(title: category_data['title'])
+    puts "> Creating questions and answers for Category #{category.title} \n\n"
+    create_questions(category, category_data['questions'])
   end
 end
